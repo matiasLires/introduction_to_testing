d6e52e23ecd4562479af48969e78f3df8d206e40
matiasLires <lires.matiash@gmail.com>
2022-10-30 20:09:12 -0300

    implement setups

--- demos/src/02-math.test.js
+++ demos/src/02-math.test.js
@@ -1,16 +1,22 @@
 const { sum, multiply, divide } = require('./02-math');
 
-test('adds 1 + 3 to equal 4', () => {
-	const res = sum(1, 3);
-	expect(res).toBe(4);
-});
-
-test('multiply 1 * 2 to equal 2', () => {
-	const res = multiply(1, 2);
-	expect(res).toBe(2);
-});
-
-test('divide 6 / 2 to equal 3', () => {
-	const res = divide(6, 2);
-	expect(res).toBe(3);
+describe('Test for math', () => {
+	describe('tests for sum', () => {
+		test('adds 1 + 3 should be 4', () => {
+			const res = sum(1, 3);
+			expect(res).toBe(4);
+		});
+	});
+	describe('tests for multiply', () => {
+		test('multiply 1 * 2 should be 2', () => {
+			const res = multiply(1, 2);
+			expect(res).toBe(2);
+		});
+	});
+	describe('tests for divide', () => {
+		test('divide 6 / 2 should be 3', () => {
+			const res = divide(6, 2);
+			expect(res).toBe(3);
+		});
+	});
 });
