60fa751ed7b7c4cf87086f081afad02e4cde66bc
matiasLires <lires.matiash@gmail.com>
2022-10-31 02:38:11 -0300

    dependencies supplantation

--- api/src/services/books.service.test.js
+++ api/src/services/books.service.test.js
@@ -1,9 +1,26 @@
 const BooksService = require('./books.service');
 
+const fakeBooks = [
+	{
+		_id: 1,
+		name: 'Harry Potter',
+	},
+];
+
+const MongoLibStub = {
+	getAll: () => [...fakeBooks],
+	create: () => {},
+};
+
+jest.mock('../lib/mongo.lib', () =>
+	jest.fn().mockImplementation(() => MongoLibStub)
+);
+
 describe('Test for BooksService', () => {
 	let service;
 	beforeEach(() => {
 		service = new BooksService();
+		jest.clearAllMocks();
 	});
 
 	describe('Test for getBooks', () => {
@@ -13,7 +30,16 @@ describe('Test for BooksService', () => {
 			const books = await service.getBooks({});
 			console.log(books);
 			// Assert
-			expect(books.length).toEqual(2);
+			expect(books.length).toEqual(1);
+		});
+
+		test('should return a name', async () => {
+			// Arrange
+			// Act
+			const books = await service.getBooks({});
+			console.log(books);
+			// Assert
+			expect(books[0].name).toEqual('Harry Potter');
 		});
 	});
 });
